6078dc8cc1da5ea063f3e9be5338940e8a7b5429
Mayank Goyal <mayankgoyal@in.ibm.com>
2015-05-08 23:23:51 +0530

    initial commit

---
+++ Appathone/Appathone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Appathone
+//
+//  Created by Mayank Goyal on 08/05/15.
+//  Copyright (c) 2015 Mayank Goyal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Appathone/Appathone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Appathone
+//
+//  Created by Mayank Goyal on 08/05/15.
+//  Copyright (c) 2015 Mayank Goyal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
